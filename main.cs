8433e7946d57998bdcd2760252aefae021f0df08
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-11-22 19:39:50 +0000

    asd

--- main.cs
+++ main.cs
@@ -18,11 +18,12 @@ class Program
 
         Console.WriteLine(number);
         Console.WriteLine("This program gets and compares random number and user number and answer.\n");
-        
+
         Console.WriteLine("decide the number will be positive or negative.");
         Console.Write("type 0 if you want positive number\ntype 1 if you want negative number\n"); input = Convert.ToInt32(Console.ReadLine());
-        
-        if (input == 0) {
+
+        if (input == 0)
+        {
             Console.WriteLine("The number will be positive\n");
             Console.Write("Enter random number between 1 to 6 --> +"); numberInput = Convert.ToInt32(Console.ReadLine());
             if (numberInput == number)
@@ -41,7 +42,9 @@ class Program
                 Console.WriteLine("The number was : +" + number + ".");
                 Console.WriteLine("Try again!");
             }
-        } else if (input == 1) {
+        }
+        else if(input == 1)
+        {
             Console.WriteLine("\nThe number will be negative\n");
             Console.Write("Enter random number between 1 to 6 --> -"); numberInput = Convert.ToInt32(Console.ReadLine());
             if (numberInput == number)
@@ -60,7 +63,9 @@ class Program
                 Console.WriteLine("The number was : -" + number + ".");
                 Console.WriteLine("Try again!");
             }
-        } else {
+        }
+        else
+        {
             Console.WriteLine("\nTHe input was not 0 or 1!");
         }
 
